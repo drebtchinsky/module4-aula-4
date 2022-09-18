4ef2374b89f0dacdb0e94144bdb76f62045aedb0
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2022-09-18 15:32:51 -0300

    iterando times com for in e for off

--- time.js
+++ time.js
@@ -1,17 +1,46 @@
 let time = [
   {
-    nome: "",
+    nome: "alejo",
     camisa: 10,
   },
   {
-    nome: "",
-    camisa: null,
+    nome: "romario",
+    camisa: 5,
   },
   {
-    nome: "",
+    nome: "roger guedes",
     camisa: 1,
   },
 ];
-console.log(time.length);
 
+// for (let i = 0; i < time.length; i++) {
+//   console.log(i, time[i]);
+// }
+
+// for (const i in time) {
+//   console.log(i, time[i]);
+// }
+
+// for (const jogador of time) {
+//   console.log(jogador)
+// }
+
+// console.table(time);
+
+// for (const key in time) {
+//   time[key]["multiplo"] = time[key]["camisa"] * 2;
+// }
+// console.table(time);
+
+// for (const i in time) {
+//   time[i].multiplo = time[i].camisa * 2;
+// }
+// console.table(time);
+// for (const jogador of time) {
+//   jogador["multiplo"] = jogador["camisa"] * 2;
+// }
+// console.table(time);
+for (const jogador of time) {
+  jogador.multiplo = jogador.camisa * 2;
+}
 console.table(time);
