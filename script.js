470d3f7574562e58367d46e2fd0e4710b65a3ea2
Gabriel Drebtchinsky <gabrieldqcarvalho@gmail.com>
2022-09-18 14:07:52 -0300

    trabalhando objetos e vetor com futebol

---
+++ script.js
@@ -0,0 +1,11 @@
+let pessoal = {
+  nome: "Alex",
+  time: "Corinthians",
+  altura: 1.5,
+  trabalhando: true
+};
+
+console.log(pessoal['nome']);
+console.log(pessoal.time);
+console.log(pessoal);
+
---
+++ time.js
@@ -0,0 +1,17 @@
+let time = [
+  {
+    nome: "",
+    camisa: 10,
+  },
+  {
+    nome: "",
+    camisa: null,
+  },
+  {
+    nome: "",
+    camisa: 1,
+  },
+];
+console.log(time.length);
+
+console.table(time);
